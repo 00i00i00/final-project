116a516c33351e5eb955c106700f2660277b86b1
Ayaka <ayaka.hibino@gmail.com>
2019-06-12 22:02:08 -0400

    added the authentication key as a new http header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-// import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { Api } from './services/api.service';
 import { EventsComponent } from './events/events.component';
 
 //commented out for now, we can add this once it is going to be used. 
-// const appRoutes: Routes =[
-//   { path: 'events', component: EventsComponent}
-// ];
+const appRoutes: Routes =[
+  { path: 'events', component: EventsComponent}
+];
 
 
 @NgModule({
