60af3c86df89f4065544be788342adf4feae25ba
Ayaka <ayaka.hibino@gmail.com>
2019-06-18 23:43:13 -0400

    made changes to adventure component so that the favorites heart click will add to an array that will display when routed to favorites page. Also there is a solution added for automatically collapsing all other divs when one div's more info button is clicked on

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,16 +1,102 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { Api } from '../services/api.service';
 
-interface Favorites {
+interface Location {
+  city: string;
+  country: string;
+  address2: string; 
+  address3: string; 
+  state: string;
+  address1: string;
+  zip_code: string;
+  display_address: [];
+  cross_streets: string;
+}
 
+interface Categories {
+  alias: string;
+  title: string;
+}
+
+interface Businesses {
+  alias: string; 
+  categories: Categories[];
+  id: string; 
+  rating: number;
+  price: string;
+  display_phone: string; 
+  name: string; 
+  url: string;
+  image_url: string;
+  location: Location[];
+  distance: number;
+  review_count: number;
+  info: boolean;
+  favorite: boolean;
 }
 
 interface ApiData {
-    page: number;
-    results: Favorites[];
-    total_results: number;
-    total_pages: number;
+  total: number;
+  businesses: Businesses[];
+  region: {
+    center: {
+      latitude:number;
+      longitude: number;
+    }
+  }
+}
+
+interface User {
+  id: string;
+  profile_url: string;
+  image_url: string;
+  name: string;
+}
+
+interface ReviewData {
+  total: number;
+  reviews: Reviews[];
+}
+
+interface Reviews {
+  id: string;
+  text: string;
+  url: string;
+  rating: number;
+  time_created: string;
+  user: User[];
+}
+
+interface BusinessDetails {
+  categories: Categories[];
+  display_phone: string;
+  hours: Hours[];
+  id: string;
+  alias: string;
+  img_url: string;
+  location: Location[];
+  name: string;
+  phone: string;
+  photos: object[];
+  price: string;
+  rating: number;
+  review_count: number;
+  url: string;
+  info: boolean;
+  
+  // attributes: object;
+}
+
+interface Hours {
+  is_open_now: boolean;
+  open: Open[];
+}
+
+interface Open {
+  day: number;
+  start: string;
+  end: string;
 }
 
 @Component({
@@ -20,12 +106,68 @@ interface ApiData {
 })
 
 export class FavoritesComponent implements OnInit{
-    list: Favorites[];
-    event: any;
+  location: string;
+  business: any;
+  list: Businesses[];
+  id: Businesses[];
+  reviews: Reviews[];
+  review: any;
+  info: boolean = false;
+  reviewId: Businesses[];
+  hours: Hours[];
+  hour: any;
+  open: Open[];
+  times: any;
+  day: any;
+  dates: any = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday' };
+  favorite: boolean;
+  favoritesList: Businesses[];
+  biz: any;
+  category: any;
+  categories: Categories[];
 
-    constructor(private api: Api, private route: ActivatedRoute){}
+  constructor(private api: Api, private route: ActivatedRoute, private router: Router){}
+
+  ngOnInit() {
+    this.api.location.subscribe(data => {
+    console.log(data);
+    this.location = data;
+    });
     
-    ngOnInit() {
-        // this.api.ticketList.subscribe(list => this.list = list);
-    }
+   this.api.favoriteList.subscribe(list => {
+    this.favoritesList = list;
+    console.log(this.favoritesList);
+    this.categories = list[0].categories;
+    console.log('categories', this.categories);
+
+   });
+  }
+
+  moreInfo = (id, business) => {
+
+      
+      this.api.getBusinessDetails(id).subscribe((data:BusinessDetails) => {
+          console.log(`API Call: Business Details from id`, data);
+          this.hours = data.hours;
+          this.open = data.hours[0].open;
+          //above means now this.open is the array of objects
+          this.day = this.open[0].day;
+        });
+        
+        this.api.getReviews(id).subscribe((data:ReviewData) => {
+            console.log(`API Call: Reviews from id`, data);
+            this.reviews = data.reviews;
+        });
+        
+        business.info = !business.info;
+}
+
+favoriteBusiness = business => {
+//   business.favorite = !business.favorite;
+    const index = this.favoritesList.indexOf(business);
+    console.log('index', index);
+    this.favoritesList.splice(index, 1);
+    this.api.updateFavorites(this.favoritesList);
+}
+
 }
